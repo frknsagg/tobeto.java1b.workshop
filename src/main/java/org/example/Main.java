36257b72e75b8eb127bfec06a20cb9a9320ea963
frknsagg <furkansag_21@hotmail.com>
2023-10-23 15:11:00 +0300

    variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,36 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // değişken isimlendirmeleri java'da camelCase yazılır
+        String ortaMetin = "İlginizi çekebilir";
+        String altMetin ="Vade süresi";
+
+        System.out.println(ortaMetin);
+
+        //integer
+        int vade = 12;
+
+        double dolarDun = 18.20;
+        double dolarBugun = 18.25;
+
+        boolean dolarDustuMu = false;
+
+        String okYonu;
+
+        if (dolarBugun<dolarDun){
+            okYonu="down.svg";
+            System.out.println(okYonu);
+        }
+        else if (dolarBugun>dolarDun){
+            okYonu="up.svg";
+            System.out.println(okYonu);
+        }
+        else {
+            okYonu="equal.svg";
+            System.out.println(okYonu);
+        }
+
+
     }
 }
